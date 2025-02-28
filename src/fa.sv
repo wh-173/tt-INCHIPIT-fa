b38a1a26e52c23a3009618fe4eb25f9ae8dc86bb
wh-173 <164562902+wh-173@users.noreply.github.com>
2025-02-28 12:23:46 +0100

    Update fa.sv

--- src/fa.sv
+++ src/fa.sv
@@ -13,9 +13,10 @@ module fa_module
     logic i0, i1, i2;
 
     xor u0(i0, A, B);
+    xor u1(S,i0, Cin);
 
     and u2(i1, A, B);
-
+    and u3(i2, i0, Cin);
     or u4(Cout, i1, i2);
 
 endmodule: fa_module
